4837ee278b866c71b0f96429c68dcd1896d5a952
Vance Miller <vbmiller@cs.unc.edu>
2014-12-06 12:50:01 -0500

    fixed resize bug

--- javascript/ui.js
+++ javascript/ui.js
@@ -4,14 +4,12 @@
 
 $(window).resize(function() {
     if ($(this).height() > 400) {
-        $("ol#playlist").css("height", $(this).height() - $("ol#playlist").offset().top - 60);
-
         if ($(".controls-container").css("float") == "left") {
             $(".controls-container").css("margin-top", $(this).height() / 2 - $(".controls-container").height());
         } else {
             $(".controls-container").css("margin-top", 0);
         }
-
+        $("ol#playlist").css("height", $(this).height() - $("ol#playlist").offset().top - 60);
     }
     });
 
